78dc21f8d0d60ebf1a9f1fa12737dd22d1a89cc6
Almuth Meier <almuth.meier@uni-oldenburg.de>
2018-05-18 12:06:15 +0200

    refactored RCS implementation

--- DynOpt/code/metrics/metrics_dynea.py
+++ DynOpt/code/metrics/metrics_dynea.py
@@ -65,7 +65,7 @@ def arr(generations_of_chgperiods, global_opt_fit_per_chgperiod, best_found_fit_
     return sum_over_changes
 
 
-def __best_of_generation(best_of_generation_per_run):
+def __best_of_generation(best_found_fit_per_gen_and_run):
     '''
     Internal function. Called by avg_best_of_generation.
 
@@ -73,16 +73,16 @@ def __best_of_generation(best_of_generation_per_run):
     "Evolutionary dynamic optimization: A survey of the state of the art"
     by Trung Thanh Nguyen et al. 2012.
 
-    @param best_of_generation_per_run: 2d numpy array containing for each run 
+    @param best_found_fit_per_gen: 2d numpy array containing for each run 
     one row that contains per generation the best achieved fitness value
-    @return: 1d numpy array: for each generation the best (i.e. minimum) fitness
+    @return: 1d numpy array: for each generation the average best fitness
     '''
 
     # average the fitness over the runs (for each generation), i.e. column-wise
-    return np.average(best_of_generation_per_run, axis=0)
+    return np.average(best_found_fit_per_gen_and_run, axis=0)
 
 
-def avg_best_of_generation(best_of_generation_per_run):
+def avg_best_of_generation(best_found_fit_per_gen_and_run):
     '''
     Average Best-of-generation measure according to description in the paper 
     "Evolutionary dynamic optimization: A survey of the state of the art"
@@ -90,13 +90,13 @@ def avg_best_of_generation(best_of_generation_per_run):
 
     Averages the Best-of-generation over all generations resulting in a scalar.
 
-    @param best_of_generation_per_run: 2d numpy array containing for each run 
+    @param best_found_fit_per_gen_and_run: 2d numpy array containing for each run 
     one row that contains per generation the best achieved fitness value
     @return: Tupel of two scalar values: (avg_bog, standard deviation of bog)
     '''
 
-    bog = __best_of_generation(best_of_generation_per_run)
-    return np.average(bog), np.std(bog)
+    bog_per_gen = __best_of_generation(best_found_fit_per_gen_and_run)
+    return np.average(bog_per_gen), np.std(bog_per_gen)
 
 
 def normalized_bog(avg_bog_per_alg_and_problem):
@@ -193,7 +193,7 @@ def best_error_before_change(generations_of_chgperiods, global_opt_fit_per_chgpe
     return sum_of_errors / n_chgperiods
 
 
-def conv_speed(gens_of_chgs, optima_of_changes, best_fit_evals_per_alg):
+def conv_speed(generations_of_chgperiods, global_opt_fit_per_chgperiod, best_found_fit_per_gen_and_alg):
     '''
     Measure of relative convergence speed of algorithms for one run of a 
     specific problem. Depends on the worst fitness value any algorithm achieved.
@@ -205,64 +205,68 @@ def conv_speed(gens_of_chgs, optima_of_changes, best_fit_evals_per_alg):
 
     Function is called in statistical_tests.py
 
-    @param gens_of_chgs_per_alg: dictionary containing a dictionary that 
-    contains the generation number for every change 
-    @param optima_of_gens: 1d numpy array containing for each generation the 
-    optimum fitness TODO falsch benannt: ist optimum per change
-    @param best_fit_evals_per_alg: dictionary containing for each algorithm a 
-    1d numpy array that contains the best fitness evaluation of each generation
+    @param generations_of_chgperiods:  dictionary containing for each change 
+    period (the real ones, not only those the EA has detected) a list with the 
+    generation numbers
+    @param global_opt_fit_per_chgperiod: 1d numpy array: for each change period
+    the global optimum fitness (for all changes stored in the dataset 
+    file, not only for those used in the experiments, i.e. 
+    len(global_opt_fit_per_chgperiod) may be larger than len(generations_of_chgperiods)
+    @param best_found_fit_per_gen_and_alg: dictionary containing for each 
+    algorithm a 1d numpy array that contains the best found fitness of each
+    generation 
     @return: dictionary containing one score for each algorithm:
         Score 0 -> best algorithm
         Score 1 -> worst algorithm
     '''
-    n_changes = len(gens_of_chgs)
-    assert n_changes == len(optima_of_changes), "inconsistency: n_changes: " + str(
-        n_changes) + " but len(optima_of_changes): " + str(len(optima_of_changes))
+    n_chgperiods = len(generations_of_chgperiods)
+    # assert n_chgperiods == len(global_opt_fit_per_chgperiod), "inconsistency: n_chgperiods: " + str(
+    # n_chgperiods) + " but len(global_opt_fit_per_chgperiod): " +
+    # str(len(global_opt_fit_per_chgperiod))
 
     # -------------------------------------------------------------------------
     # compute worst fitness per change achieved by any algorithm
 
     # 2d list: one row for each algorithm
-    evals_per_alg_list = list(best_fit_evals_per_alg.values())
+    evals_per_alg_list = list(best_found_fit_per_gen_and_alg.values())
     # worst fitness per generation achieved by any algorithm
     worst_fit_evals = np.max(evals_per_alg_list, axis=0)
     # compute worst fitness per change
-    worst_fit_per_change = {}
-    # dictionary containing the fitness of the global optimum for each change
-    global_opt_fit_per_change = {}
-    for chg_nr, gens in gens_of_chgs.items():
-        worst_fit_per_change[chg_nr] = np.max(worst_fit_evals[gens])
-        # all generations with indices in "gens" have same global opt. fitness
-        #global_opt_fit_per_change[chg_nr] = optima_of_gens[gens[0]]
-        global_opt_fit_per_change[chg_nr] = optima_of_changes[chg_nr]
+    worst_fit_per_chgperiod = {}
+    for chgperiod_nr, gens in generations_of_chgperiods.items():
+        worst_fit_per_chgperiod[chgperiod_nr] = np.max(worst_fit_evals[gens])
     # test whether worst fitness is larger than global best fitness
-    assert len(list(global_opt_fit_per_change.values())) == len(list(worst_fit_per_change.values())), "len-opt: " + \
-        str(len(list(global_opt_fit_per_change.values()))) + " len-worst: " + \
-        str(len(list(worst_fit_per_change.values())))
+    # assert len(global_opt_fit_per_chgperiod) == len(list(worst_fit_per_chgperiod.values())), "len-opt: " + \
+    #    str(len(global_opt_fit_per_chgperiod)) + " len-worst: " + \
+    #    str(len(list(worst_fit_per_chgperiod.values())))
     try:
-        assert list(worst_fit_per_change.values()) >= list(
-            global_opt_fit_per_change.values()), "global fitness worse than worst fitness"
+        assert np.all(list(worst_fit_per_chgperiod.values(
+        )) >= global_opt_fit_per_chgperiod[range(n_chgperiods)]), "global fitness worse than worst fitness"
     except Exception as e:
         print(e, flush=True)
-        print("worst-fit-per-change: ")
-        print(list(worst_fit_per_change.values()))
+        print("worst-fit-per-change-period: ")
+        print(list(worst_fit_per_chgperiod.values()))
         print()
-        print("global-opt-fit: ")
-        print(list(global_opt_fit_per_change.values()))
+        print("global-opt-fit-per-change-period: ")
+        print(global_opt_fit_per_chgperiod)
+        raise  # throw the exception
 
     # -------------------------------------------------------------------------
     # compute convergence speed for each algorithm
     speed_per_alg = {}
-    algs = list(best_fit_evals_per_alg.keys())
+    algs = list(best_found_fit_per_gen_and_alg.keys())
     for alg in algs:
-        speed_per_alg[alg] = __convergence_speed__(gens_of_chgs,
-                                                   global_opt_fit_per_change,
-                                                   best_fit_evals_per_alg[alg],
-                                                   worst_fit_per_change)
+        speed_per_alg[alg] = __convergence_speed__(generations_of_chgperiods,
+                                                   global_opt_fit_per_chgperiod,
+                                                   best_found_fit_per_gen_and_alg[alg],
+                                                   worst_fit_per_chgperiod)
     return speed_per_alg
 
 
-def __convergence_speed__(generations_of_changes, optima_of_changes, best_fitness_evals, worst_fit_per_change):
+def __convergence_speed__(generations_of_chgperiods,
+                          global_opt_fit_per_chgperiod,
+                          best_found_fit_per_gen,
+                          worst_fit_per_chgperiod):
     '''
     Internal method, called by conv_speed().
 
@@ -272,25 +276,32 @@ def __convergence_speed__(generations_of_changes, optima_of_changes, best_fitnes
     as formally defined should be able to handle maximization problems as well)
     Between 0 and 1. Best case: 0, worst case: 1.
 
-    @param best_fitness_evals: 1d numpy array containing for each generation the
-    best fitness value achieved by this algorithm.
-    @param worst_fit_per_change: dictionary containing for each change period 
+    @param generations_of_chgperiods:  dictionary containing for each change 
+    period (the real ones, not only those the EA has detected) a list with the 
+    generation numbers
+    @param global_opt_fit_per_chgperiod: 1d numpy array: for each change period
+    the global optimum fitness (for all changes stored in the dataset 
+    file, not only for those used in the experiments, i.e. 
+    len(global_opt_fit_per_chgperiod) may be larger than len(generations_of_chgperiods)
+    @param best_found_fit_per_gen: 1d numpy array containing for each generation 
+    the best fitness value achieved by this algorithm.
+    @param worst_fit_per_chgperiod: dictionary containing for each change period 
     the worst fitness value achieved by any algorithm.
     @return: scalar: convergence speed for this algorithm
     '''
     sum_norm_areas = 0
-    for chg_nr, gens in generations_of_changes.items():
-        optimal_fit = optima_of_changes[chg_nr]
-        worst_fit = worst_fit_per_change[chg_nr]
+    for chgperiod_nr, gens in generations_of_chgperiods.items():
+        optimal_fit = global_opt_fit_per_chgperiod[chgperiod_nr]
+        worst_fit = worst_fit_per_chgperiod[chgperiod_nr]
         best_worst_fit_diff = abs(optimal_fit - worst_fit)
         # compute area for this change
         area_for_change = 0
         max_area_for_change = 0
         gen_in_chg = 0
         for gen in gens:
-            assert optimal_fit <= best_fitness_evals[gen], "opt-fit " + str(
-                optimal_fit) + " fit " + str(best_fitness_evals[gen])
-            diff = abs(optimal_fit - best_fitness_evals[gen])
+            assert optimal_fit <= best_found_fit_per_gen[gen], "opt-fit " + str(
+                optimal_fit) + " fit " + str(best_found_fit_per_gen[gen])
+            diff = abs(optimal_fit - best_found_fit_per_gen[gen])
             area_for_change += (gen_in_chg + 1) * diff  # +1, otherwise first 0
             max_area_for_change += (gen_in_chg + 1) * best_worst_fit_diff
             gen_in_chg += 1
@@ -302,4 +313,4 @@ def __convergence_speed__(generations_of_changes, optima_of_changes, best_fitnes
             norm_area_for_change = area_for_change / max_area_for_change
             sum_norm_areas += norm_area_for_change
 
-    return sum_norm_areas / len(generations_of_changes)
+    return sum_norm_areas / len(generations_of_chgperiods)
