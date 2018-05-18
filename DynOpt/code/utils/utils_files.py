caf3d277b84ad02101ca26a6cd9d5258789c8638
Almuth Meier <almuth.meier@uni-oldenburg.de>
2018-05-18 11:23:16 +0200

    bugfix: false positions of parantheses led to false selection of output files

--- DynOpt/code/utils/utils_files.py
+++ DynOpt/code/utils/utils_files.py
@@ -91,7 +91,7 @@ def get_array_file_names_for_experiment_file_name(exp_file_name, arrays_path):
             arrays_path, f)) and f.endswith('.npz') and function in f
             and ("_d-" + str(dim) + "_") in f and ("_pch-" + pch + "_")in f
             and ("_fch-" + fch + "_") in f)]
-    elif function == "mpbnoisy" or function == "mpbrandom":
+    elif function == "mpbnoisy" or function == "mpbrand":
         veclen = splitted_benchmark_file_name[3].split('-')[1]
         peaks = splitted_benchmark_file_name[4].split('-')[1]
         noise = splitted_benchmark_file_name[5].split('-')[1]
@@ -124,11 +124,11 @@ def select_experiment_files(benchmark_path, benchmarkfunction, poschgtypes,
             benchmarkfunction == "rosenbrock":
         selected_exp_files = [f for f in all_experiment_files if (
                               any(("_d-" + str(dim) + "_") in f for dim in dims) and
-                              any(("_pch-" + poschgtype) + "_" in f for poschgtype in poschgtypes) and
-                              any(("_fch-" + fitchgtype) + "_" in f for fitchgtype in fitchgtypes))]
+                              any(("_pch-" + poschgtype + "_") in f for poschgtype in poschgtypes) and
+                              any(("_fch-" + fitchgtype + "_") in f for fitchgtype in fitchgtypes))]
     elif benchmarkfunction == "mpbnoisy" or \
-            benchmarkfunction == "mpbrandom":
+            benchmarkfunction == "mpbrand":
         selected_exp_files = [f for f in all_experiment_files if (
-                              any(("_d-" + str(dim) + "_" in f for dim in dims) and
-                                  ("_noise-" + str(noise) + "_") in f for noise in noises))]
+                              any(("_d-" + str(dim) + "_") in f for dim in dims) and
+                              any(("_noise-" + str(noise) + "_") in f for noise in noises))]
     return selected_exp_files
