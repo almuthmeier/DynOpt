38b37fe86f10ca3e40e38ffb315bcdd0fa28ab28
Almuth Meier <almuth.meier@uni-oldenburg.de>
2018-05-25 09:25:21 +0200

    created class for statistical tests

---
+++ DynOpt/code/metrics/stattest_calculator.py
@@ -0,0 +1,18 @@
+'''
+Created on May 18, 2018
+
+@author: ameier
+'''
+
+
+class StatisticalTestsCalculator():
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
