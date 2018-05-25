42a675092de4ebfc01ab25763962203299671b36
Almuth Meier <almuth.meier@uni-oldenburg.de>
2018-05-25 11:00:45 +0200

    created class für dynamic cmaes

---
+++ DynOpt/code/algorithms/dyncmaes.py
@@ -0,0 +1,16 @@
+'''
+Created on May 25, 2018
+
+@author: ameier
+'''
+
+
+class DynamicCMAES(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
