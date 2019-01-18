47f7c6f9620515d58f4a9bad071e56a42134cdc7
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 00:29:11 +0100

    mehr kernel sizes

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kernelsizes=(2 3 4 5)
+kernelsizes=(2 3 4 5 6 7)
 for k in "${kernelsizes[@]}"
 do
 	sbatch --job-name="ks-$k" --output="slurm_ks-$k.%j.out" --error="slurm_ks-$k.%j.err" subscript.job $k &
