e23f20a0842a55740c9804d315f7c4377d84ad5b
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 17:25:55 +0100

    problem with empty input params

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -11,8 +11,8 @@ algnameaddition2="_auto"
 
 for d in "${dims[@]}"
 do
-	sbatch --job-name="d$d-tcn" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job $pred $algnameaddition1 $d &
-	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job $pred $algnameaddition2 $d &
+	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred" "$algnameaddition1" "$d" &
+	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred" "$algnameaddition2" "$d" &
 done
 
 wait
