c7fcacf7e561bfe44b19e2508482e4b8b28e9ca2
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 22:48:03 +0100

    mpbcorr, 20 repetitions (Nevada); change job name so that they do not overwrite the output of SRR

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -31,8 +31,8 @@ useuncs2="True"
 
 for d in "${dims[@]}"
 do
-	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$d" &
-	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$d" &
+	sbatch --job-name="Md$d-tcn" --output="slurm_mpbcorr_d$d-tcn.%j.out" --error="slurm_mpbcorr_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$d" &
+	sbatch --job-name="Md$d-auto" --output="slurm_mpbcorr_d$d-auto.%j.out" --error="slurm_mpbcorr_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$d" &
 done
 
 wait
