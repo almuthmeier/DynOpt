49d511fc50e33eec21611a4eab04896abc66e5f6
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 17:53:23 +0100

    scripts and paths for Vegas

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -5,14 +5,18 @@ dims=(2 5 10 20)
 
 
 
-pred="tcn"
+pred1="no"
+pred2="autoregressive"
 algnameaddition1=""
-algnameaddition2="_auto"
+algnameaddition2=""
+
+#sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$d" &
+#sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$d" &
 
 for d in "${dims[@]}"
 do
-	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred" "$algnameaddition1" "$d" &
-	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred" "$algnameaddition2" "$d" &
+	./subscript2.job "$pred1" "$algnameaddition1" "$d" &
+	./subscript2.job "$pred2" "$algnameaddition2" "$d" &
 done
 
 wait
