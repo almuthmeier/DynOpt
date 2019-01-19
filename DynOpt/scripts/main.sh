85a4a1f9388f68fd6c6567a070eb3b20f97716da
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 21:41:58 +0100

    Bugfix: AutoTCN was used withount Uncertainties

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -10,9 +10,11 @@ dims=(2 5 10 20)
 #pred2="autoregressive"
 #algnameaddition1=""
 #algnameaddition2=""
+#useuncs1="False"
+#useuncs2="False"
 
-#./subscript2.job "$pred1" "$algnameaddition1" "$d" &
-#./subscript2.job "$pred2" "$algnameaddition2" "$d" &
+#./subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$d" &
+#./subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$d" &
 
 # ----------------------------------------------------------------------------
 # tcn, autotcn
@@ -21,14 +23,16 @@ pred1="tcn"
 pred2="tcn"
 algnameaddition1=""
 algnameaddition2="_auto"
+useuncs1="False"
+useuncs2="True"
 
 # ----------------------------------------------------------------------------
 
 
 for d in "${dims[@]}"
 do
-	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$d" &
-	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$d" &
+	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$d" &
+	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$d" &
 done
 
 wait
