e17e0ea270220718e9b0c77ec5e802b5d8f57be9
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-19 20:47:17 +0100

    scripts for mpbcorr (Nevada)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -3,20 +3,32 @@
 dims=(2 5 10 20)
 
 
+# ----------------------------------------------------------------------------
+# no, ar
 
+#pred1="no"
+#pred2="autoregressive"
+#algnameaddition1=""
+#algnameaddition2=""
 
-pred1="no"
-pred2="autoregressive"
+#./subscript2.job "$pred1" "$algnameaddition1" "$d" &
+#./subscript2.job "$pred2" "$algnameaddition2" "$d" &
+
+# ----------------------------------------------------------------------------
+# tcn, autotcn
+
+pred1="tcn"
+pred2="tcn"
 algnameaddition1=""
-algnameaddition2=""
+algnameaddition2="_auto"
 
-#sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$d" &
-#sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$d" &
+# ----------------------------------------------------------------------------
+
 
 for d in "${dims[@]}"
 do
-	./subscript2.job "$pred1" "$algnameaddition1" "$d" &
-	./subscript2.job "$pred2" "$algnameaddition2" "$d" &
+	sbatch --job-name="d$d-tcn" --output="slurm_d$d-tcn.%j.out" --error="slurm_d$d-tcn.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$d" &
+	sbatch --job-name="d$d-auto" --output="slurm_d$d-auto.%j.out" --error="slurm_d$d-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$d" &
 done
 
 wait
