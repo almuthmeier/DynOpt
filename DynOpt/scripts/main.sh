38b23140e8e9fd60bf1bbb0a211df63f8a0d8c58
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-22 23:11:59 +0100

    SRR for 1*sigma and sigma=rmse(found-pred)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -22,13 +22,13 @@ dims=(2 5 10 20)
 pred1="tcn"
 pred2="tcn"
 algnameaddition1=""								# TCN
-algnameaddition2a="_auto_00-8"				# AutoTCN variants
-algnameaddition2b="_auto_08-0"
+algnameaddition2a="_auto_1sig"				# AutoTCN variants
+algnameaddition2b="_auto_rmse"
 useuncs1="False"
 useuncs2="True"
 epuncfactor1=0.0
-epuncfactor2a=0.01 #(0.8%)
-epuncfactor2b=0.1 #(8%)
+epuncfactor2a=1 
+epuncfactor2b=999 #rmse
 
 
 
@@ -49,13 +49,16 @@ epuncfactor2b=0.1 #(8%)
 ## different sigmas
 #sbatch --mem=45G --job-name="Md$d-a-auto" --output="slurm_mpbcorr_d$d-a-auto.%j.out" --error="slurm_mpbcorr_d$d-a-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2a" "$useuncs2" "$epuncfactor2a" "$d" &
 #sbatch --mem=45G --job-name="Md$d-b-auto" --output="slurm_mpbcorr_d$d-b-auto.%j.out" --error="slurm_mpbcorr_d$d-b-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2b" "$useuncs2" "$epuncfactor2b" "$d" &
+
+#sbatch --mem=45G --job-name="d$d-001-auto" --output="slurm_d$d-001-auto.%j.out" --error="slurm_d$d-001-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2a" "$useuncs2" "$epuncfactor2a" "$d" &
+#sbatch --mem=45G --job-name="d$d-01-auto" --output="slurm_d$d-01-auto.%j.out" --error="slurm_d$d-01-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2b" "$useuncs2" "$epuncfactor2b" "$d" &
 # ----------------------------------------------------------------------------
 
 
 for d in "${dims[@]}"
 do
-	sbatch --mem=45G --job-name="d$d-001-auto" --output="slurm_d$d-001-auto.%j.out" --error="slurm_d$d-001-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2a" "$useuncs2" "$epuncfactor2a" "$d" &
-	sbatch --mem=45G --job-name="d$d-01-auto" --output="slurm_d$d-01-auto.%j.out" --error="slurm_d$d-01-auto.%j.err" subscript2.job "$pred2" "$algnameaddition2b" "$useuncs2" "$epuncfactor2b" "$d" &
+	sbatch --mem=45G --job-name="d$d-1sig_auto" --output="slurm_d$d-1sig_auto.%j.out" --error="slurm_d$d-1sig_auto.%j.err" subscript2.job "$pred2" "$algnameaddition2a" "$useuncs2" "$epuncfactor2a" "$d" &
+	sbatch --mem=45G --job-name="d$d-rmse_auto" --output="slurm_d$d-rmse_auto.%j.out" --error="slurm_d$d-rmse_auto.%j.err" subscript2.job "$pred2" "$algnameaddition2b" "$useuncs2" "$epuncfactor2b" "$d" &
 done
 
 wait
