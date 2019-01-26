a0e4e2530d84af90d071d0bda9719a73416c1723
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-01-26 22:11:59 +0100

    mpbcorr: ar pRND, nDEV (Vegas)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -4,18 +4,23 @@ dims=(2 5 10 20)
 zfactors=0.01,0.1,1.0,10.0
 epuncfactor=0.0 # unused
 
+
+# ----------------------------------------------------------------------------
+
+
+pred1="autoregressive"
+pred2="autoregressive"
+algnameaddition1="_predRND" # for autoTCN: _auto_ ... !!!
+algnameaddition2="_predDEV" 								
+useuncs1="False"
+useuncs2="False"
+reinimode1="pred-RND"
+reinimode2="pred-DEV"
+
+
 # ----------------------------------------------------------------------------
 # no, ar
 
-#pred1="kalman"
-#pred2="kalman"
-#algnameaddition1="_predRND"
-#algnameaddition2="_predDEV"
-#useuncs1="True"
-#useuncs2="True"
-#epuncfactor=0.0		# unused
-#reinimode1="pred-RND"
-#reinimode2="pred-DEV"
 
 #./subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$epuncfactor" "$reinimode1" "$zfactors" "$d" &
 #./subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$epuncfactor" "$reinimode2" "$zfactors" "$d" &
@@ -25,14 +30,6 @@ epuncfactor=0.0 # unused
 # ----------------------------------------------------------------------------
 # tcn, autotcn
 
-pred1="tcn"
-pred2="tcn"
-algnameaddition1="_predRND" # for autoTCN: _auto_ ... !!!
-algnameaddition2="_predDEV" 								
-useuncs1="False"
-useuncs2="False"
-reinimode1="pred-RND"
-reinimode2="pred-DEV"
 
 
 # for sphere
@@ -49,6 +46,9 @@ reinimode2="pred-DEV"
 
 # 24.1. (pred-DEV reinitialization type)
 #sbatch --mem=35G --job-name="d$d-tcn_predDEV" --output="slurm_d$d-tcn_predDEV.%j.out" --error="slurm_d$d-tcn_predDEV.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$epuncfactor1" "$reinimode" "$zfactors" "$d" &
+
+#sbatch --mem=24G --job-name="d$d-t_RND" --output="slurm_d$d-t_RND.%j.out" --error="slurm_d$d-t_RND.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$epuncfactor" "$reinimode1" "$zfactors" "$d" &
+#sbatch --mem=24G --job-name="d$d-t_DEV" --output="slurm_d$d-t_DEV.%j.out" --error="slurm_d$d-t_RND.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$epuncfactor" "$reinimode2" "$zfactors" "$d" &
 
 
 # for mpb (different jobnames)
@@ -69,9 +69,9 @@ reinimode2="pred-DEV"
 
 
 for d in "${dims[@]}"
-do
-	sbatch --mem=24G --job-name="d$d-t_RND" --output="slurm_d$d-t_RND.%j.out" --error="slurm_d$d-t_RND.%j.err" subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$epuncfactor" "$reinimode1" "$zfactors" "$d" &
-	sbatch --mem=24G --job-name="d$d-t_DEV" --output="slurm_d$d-t_DEV.%j.out" --error="slurm_d$d-t_RND.%j.err" subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$epuncfactor" "$reinimode2" "$zfactors" "$d" &
+do	
+	./subscript2.job "$pred1" "$algnameaddition1" "$useuncs1" "$epuncfactor" "$reinimode1" "$zfactors" "$d" &
+	./subscript2.job "$pred2" "$algnameaddition2" "$useuncs2" "$epuncfactor" "$reinimode2" "$zfactors" "$d" &
 done
 
 wait
