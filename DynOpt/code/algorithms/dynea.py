Bugfix: multivariate_normal actually takes (co)variace but not standard deviation -> removed sqrt on variance (for pKal and pUnc)

DynOpt/code/algorithms/dynea.py
@@ -263,12 +263,13 @@ class DynamicEA():
         Generates immigrants around the predected optimum, following the
         current re-initialization strategy.
 
-        @param z: factor for the sigma environment
+        @param z: factor for the interval
         @param pred_optimum_position: predicted optimum
         @param n_immigrants_per_noise: (int) number of immigrants per noise level
         '''
         mean = 0.0
-        sigma = None
+        covariance = None  # covariance (matrix) or variance (vector)
+        sigma = None  # standard deviation (scalar)
         try:
             n_preds, _ = self.pred_opt_pos_per_chgperiod.shape
         except AttributeError:
@@ -280,17 +281,17 @@ class DynamicEA():
             # -> one sigma for all dimensions
             sigma = 1.0
         elif self.reinitialization_mode == "pred-UNC":
-            # convert predictive variance to standard deviation
-            # -> different sigma for each dimension
+            # predictive variance for re-initialization
+            # -> different variance for each dimension
             try:
                 # for predictor "tcn" (AutoTCN)
-                sigma = np.sqrt(self.epist_unc_per_chgperiod[-1])
+                covariance = self.epist_unc_per_chgperiod[-1] # variance (vector)
             except:
                 # for predictor "kalman"
-                sigma = np.sqrt(self.kal_variance_per_chgperiod[-1])
-            assert len(sigma) == self.dim
+                covariance = self.kal_variance_per_chgperiod[-1] # variance (vector)
+            assert len(covariance) == self.dim
         elif self.reinitialization_mode == "pred-DEV":
-            # -> one sigma for all dimensions
+            # -> one variance for all dimensions
             # difference of the last prediction and the last best found position;
             # average over all dimensions
 
@@ -315,18 +316,17 @@ class DynamicEA():
             c = 0.1  # seemed to be good setting in the paper
             try:
                 # for predictor "kalman"
-                variance = self.kal_variance_per_chgperiod[-1]
+                covariance = self.kal_variance_per_chgperiod[-1]
             except:
                 # for predictor "tcn" (AutoTCN)
-                variance = self.epist_unc_per_chgperiod[-1]
-            max_variance = np.max(variance)
+                covariance = self.epist_unc_per_chgperiod[-1]
+            max_variance = np.max(covariance)
             max_sigma = np.sqrt(max_variance)
             g = c / (1 + max_sigma)
             # number of immigrants produced around the prediction (remaining
             # immigrants are created randomly afterwards)
             n_immigrants_per_noise = math.floor(g * n_immigrants_per_noise)
-            sigma = np.sqrt(variance)
-            assert len(sigma) == self.dim
+            assert len(covariance) == self.dim
         else:
             warnings.warn("unknown reinitialization mode: " +
                           self.reinitialization_mode)
@@ -335,7 +335,7 @@ class DynamicEA():
             return np.array([])
         noisy_optimum_positions = np.array(
             [gaussian_mutation(pred_optimum_position, mean,
-                               z * sigma, self.pred_np_rnd_generator)
+                               sigma, self.pred_np_rnd_generator, z, covariance)
              for _ in range(n_immigrants_per_noise)])
         return noisy_optimum_positions
 

DynOpt/code/utils/utils_ea.py
@@ -53,27 +53,40 @@ def dominant_recombination(population, ro, ea_np_rnd_generator):
     return offspring
 
 
-def gaussian_mutation(x, mean, sigma, ea_np_rnd_generator):
+def gaussian_mutation(x, mean, sigma, ea_np_rnd_generator, z=None, covariance=None):
     '''
     Gaussian mutation.
 
     Mutates individual x by adding Gaussian Noise.
 
+    @param mean: (scalar)
     @param ea_np_rnd_generator: numpy random number generator, created with
         np.random.RandomState(<seed>)
+    @param sigma: (scalar) standard deviation of distribution
+    @param z: (scalar) scale of the distribution
+    @param covariance: (scalar) (co)variance; use either sigma or covariance
     '''
-    try:
-        len(sigma)  # throws exception if sigma is a scalar
-        # multivariate sigma: for each dimension different standard deviation
+    # TODO das z extra uebergeben, damit es auch bei multivariate funktioniert
+
+    if sigma is None:
+        # multivariate variance: for each dimension different variance
         # convert single float to vector by repeating it multiple times
         mean = np.array([mean] * len(x))
-        # convert vector to diagonal matrix
-        sigma = np.diag(sigma)
-        noise = ea_np_rnd_generator.multivariate_normal(mean, sigma)
-    except:
+        try:
+            # throws exception if covariance is a row vector
+            # -> contains only variance (since covariance would require matrix)
+            covariance.shape[1]
+        except IndexError:  # variance case
+            "tuple index out of range"
+            # convert variance vector to diagonal covariance matrix
+            covariance = np.diag(covariance)
+        noise = ea_np_rnd_generator.multivariate_normal(mean, covariance)
+    else:  # sigma (standard deviation) is scalar
         noise = ea_np_rnd_generator.normal(mean, sigma, len(x))
 
-    return x + noise
+    if z is None:
+        z = 1
+    return x + z * noise
 
 
 def mu_plus_lambda_selection(mu, parents, offsprings, parents_fitness, offsprings_fitness):