a1a582e03e17539fb7e96286f51e5e3414d3ba5c
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-07-25 23:18:53 +0200

    log-files used again

--- DynOpt/code/input_parser.py
+++ DynOpt/code/input_parser.py
@@ -268,7 +268,8 @@ def initialize_comparator_with_read_inputs(parser, comparator):
     n_current_inputs = len(vars(args))
 
     if n_current_inputs != 55:
-        exit("Error: input_parser.py: false number of inputs: " + str(n_current_inputs))
+        exit("Error: input_parser.py: false number of inputs: " +
+             str(n_current_inputs))
 
     # benchmark problem
     comparator.algorithm = args.algorithm
@@ -432,8 +433,8 @@ def run_parser():
     orig_stderr = sys.stderr
     f = open(
         log_file_name, 'w')
-    # sys.stdout = f  # TODO(exe) in-comment this
-    #sys.stderr = f
+    sys.stdout = f  # TODO(exe) in-comment this
+    sys.stderr = f
     #
     # =======================================================================
     # run experiments
