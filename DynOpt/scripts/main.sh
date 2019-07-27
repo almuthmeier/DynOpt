ed8ded5d2d2c28dd0945eb9d793fba54c47c9ca9
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-07-27 11:22:45 +0200

    branke again on Nevada (20-d Rosenbrock)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dims=(2 5 10 20)
-#dims=(20)
+#dims=(2 5 10 20)
+dims=(20)
 
 
 # ----------------------------------------------------------------------------
@@ -15,7 +15,7 @@ pred2="no"
 cmavariant1="predcma_internal"
 cmavariant2="predcma_internal"
 
-predvariant1="hdwom"
+predvariant1="branke"
 predvariant2="hd"
 							
 useuncs1="False"
@@ -79,8 +79,8 @@ for d in "${dims[@]}"
 do
 	#./subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
 	#./subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &	
-		sbatch --mem=30G --job-name="d$d-$predvariant1" --output="slurm_d$d-$predvariant1.%j.out" --error="slurm_d$d-$predvariant1.%j.err" subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
-		sbatch --mem=30G --job-name="d$d-$predvariant2" --output="slurm_d$d-$predvariant2.%j.out" --error="slurm_d$d-$predvariant2.%j.err" subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &
+		sbatch --mem=40G --job-name="d$d-$predvariant1" --output="slurm_d$d-$predvariant1.%j.out" --error="slurm_d$d-$predvariant1.%j.err" subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
+	#	sbatch --mem=40G --job-name="d$d-$predvariant2" --output="slurm_d$d-$predvariant2.%j.out" --error="slurm_d$d-$predvariant2.%j.err" subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &
 done
 
 
