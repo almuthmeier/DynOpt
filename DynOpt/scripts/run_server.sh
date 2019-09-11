5a0742c61c2e1866fa83c0bdeed5f75c9932ce17
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-09-11 11:06:53 +0200

    benchmark path

--- DynOpt/scripts/run_server.sh
+++ DynOpt/scripts/run_server.sh
@@ -32,9 +32,9 @@ lenchgperiod=30				# number of generations per change period; has to be
 ischgperiodrandom="False"	# True if the change occurs at a random time point.
 							# defines the benchmark function, must be located 
 							# in the datasets/ folder of this project
-benchmarkfunctionfolderpath="/raid/almuth/DynCMA/Ausgaben/data_2019-09-11/vel-0.5/"		 	# parent directory path of the benchmark functions
+benchmarkfunctionfolderpath="/raid/almuth/DynCMA/Ausgaben/data_2019-09-11_newDSB/vel-0.5/"		 	# parent directory path of the benchmark functions
 		   																				 	# and child directory of the datasets folder of this project
-#benchmarkfunctionfolderpath="/home/almuth/Documents/Promotion/Ausgaben/DynCMA/data_2019-09-11/vel-0.5/" 
+#benchmarkfunctionfolderpath="/home/almuth/Documents/Promotion/Ausgaben/DynCMA/data_2019-09-11_newDSB/vel-0.5/" 
 lbound=0					# minimum bound of the benchmark's range							   				 
 ubound=100					# maximum bound of the benchmark's range										   																				 
 
