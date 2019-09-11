de94b7240b54bb4c705af9e023620c9f86f0a491
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-09-11 11:52:49 +0200

    resetcma (Nevada)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#dims=(2 5 10 20)
-dims=(5)
-#dims=(5 10)
+dims=(2 5 10 20)
+#dims=(5)
 
 
 # ----------------------------------------------------------------------------
@@ -11,16 +10,16 @@ dims=(5)
 #predictor
 
 pred1="no"
-pred2="tcn"
+pred2="no"
 
 cmavariant1="predcma_internal"
-cmavariant2="predcma_external"
+cmavariant2="resetcma"
 
 predvariant1="branke"
-predvariant2="a"
+predvariant2="None"
 							
 useuncs1="False"
-useuncs2="True"
+useuncs2="False"
 
 
 algnameaddition1="_$predvariant1"
@@ -78,8 +77,8 @@ algnameaddition2="_$predvariant2"
 
 for d in "${dims[@]}"
 do
-	./subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
-	#./subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &	
+	#./subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
+	./subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &	
 	#	sbatch --mem=32G --job-name="d$d-$predvariant1" --output="slurm_d$d-$predvariant1.%j.out" --error="slurm_d$d-$predvariant1.%j.err" subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
 	#	sbatch --mem=32G --job-name="d$d-$predvariant2" --output="slurm_d$d-$predvariant2.%j.out" --error="slurm_d$d-$predvariant2.%j.err" subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &
 done
