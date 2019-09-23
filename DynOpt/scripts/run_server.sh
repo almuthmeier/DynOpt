4197aa32df8546e1500f755dc656110df9be8fd8
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-09-23 16:26:17 +0200

    hdwom d=5

--- DynOpt/scripts/run_server.sh
+++ DynOpt/scripts/run_server.sh
@@ -120,7 +120,7 @@ predictor=$1				# no, rnn, autoregressive, tfrnn, tftlrnn, tftlrnndense, tcn, ka
 algnameaddition=$2
 outputdirectory="$pathadditions/$algorithm""_""$predictor""$algnameaddition/"						# name of the output directory. Necessary to separate results for different algorithm settings.				
 outputdirectorypath="/raid/almuth/DynCMA/Ausgaben/output_2019-09-23_sigpred/$benchmarkfunction/"		# path to output folder
-#outputdirectorypath="/home/almuth/Documents/Promotion/Ausgaben/DynCMA/output_2019-09-23_sigpred/$benchmarkfunction/"
+#outputdirectorypath="/home/almuth/Documents/Promotion/Ausgaben/DynCMA/output_2019-09-23_sigpred_hdwom5/$benchmarkfunction/"
 #------------------------------------------------------------------------------
 
 # (There must always be a space between the argument and the backslash!)
