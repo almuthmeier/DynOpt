1c0245eedf7be6f8de2bdd2b82022c97c4bb4211
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-09-25 09:24:55 +0200

    static (vel=0.5)

--- DynOpt/scripts/main.sh
+++ DynOpt/scripts/main.sh
@@ -11,16 +11,16 @@ dims=(2 5 10 20)
 pred1="no"
 pred2="no"
 
-cmavariant1="predcma_internal"
+cmavariant1="static"
 cmavariant2="predcma_internal"
 
-predvariant1="hdwom"
+predvariant1="None"
 predvariant2="hd"
 							
 useuncs1="False"
 useuncs2="False"
 
-algnameaddition1="_$predvariant1" # "_static_$predvariant1"
+algnameaddition1="_static_$predvariant1"
 algnameaddition2="_$predvariant2"
 
 # ----------------------------------------------------------------------------
@@ -78,7 +78,7 @@ do
 	#./subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
 	#./subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &	
 		sbatch --mem=16G --job-name="d$d-$predvariant1" --output="slurm_d$d-$predvariant1.%j.out" --error="slurm_d$d-$predvariant1.%j.err" subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$cmavariant1" "$predvariant1" "$d" &
-		sbatch --mem=16G --job-name="d$d-$predvariant2" --output="slurm_d$d-$predvariant2.%j.out" --error="slurm_d$d-$predvariant2.%j.err" subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &
+	#	sbatch --mem=16G --job-name="d$d-$predvariant2" --output="slurm_d$d-$predvariant2.%j.out" --error="slurm_d$d-$predvariant2.%j.err" subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$cmavariant2" "$predvariant2" "$d" &
 done
 
 
