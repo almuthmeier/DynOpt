36e3d7a4643ba300235ee798cb30085ab01b0e97
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-10-31 18:08:21 +0100

    folder

--- DynOpt/scripts/icann_run_server.sh
+++ DynOpt/scripts/icann_run_server.sh
@@ -114,7 +114,7 @@ predictor=$1				# no, rnn, autoregressive, tfrnn, tftlrnn, tftlrnndense, tcn, ka
 
 algnameaddition=$2
 outputdirectory="$pathadditions/$algorithm""_""$predictor""$algnameaddition/"						# name of the output directory. Necessary to separate results for different algorithm settings.				
-outputdirectorypath="/raid/almuth/Uncertainty/Ausgaben/output_2019-10-31_alle_DSB/$benchmarkfunction/"		# path to output folder
+outputdirectorypath="/raid/almuth/Uncertainty/Ausgaben/output_2019-10-31_newz/$benchmarkfunction/"		# path to output folder
 #outputdirectorypath="/home/almuth/Documents/Promotion/Ausgaben/Uncertainty/output_2019-04-15/$benchmarkfunction/"
 #------------------------------------------------------------------------------
 
