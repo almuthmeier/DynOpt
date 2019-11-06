3dedc8de923617cc66f006952a3e998eb9c62232
Almuth Meier <almuth.meier@uni-oldenburg.de>
2019-11-06 10:30:44 +0100

    autoTCN (pUNC) new zfactors on MPB (more memory)

--- DynOpt/scripts/icann_main.sh
+++ DynOpt/scripts/icann_main.sh
@@ -14,14 +14,14 @@ zfactors=0.1,0.5,1.0,2.0
 
 
 #pred1="tcn"
-#pred2="tcn"
-#algnameaddition1="_auto_predDEV" # for autoTCN: _auto_ ... !!! (that is TCN with uncertainty estimate
-#algnameaddition2="_auto_predUNC" 								
-
-pred1="tcn"
 pred2="tcn"
-algnameaddition1="_predRND" 
-algnameaddition2="_predDEV"
+#algnameaddition1="_auto_predDEV" # for autoTCN: _auto_ ... !!! (that is TCN with uncertainty estimate
+algnameaddition2="_auto_predUNC" 								
+
+#pred1="tcn"
+#pred2="tcn"
+#algnameaddition1="_predRND" 
+#algnameaddition2="_predDEV"
 
 #pred1="autoregressive"
 #pred2="autoregressive"
@@ -30,9 +30,9 @@ algnameaddition2="_predDEV"
 
  								
 useuncs1="False"
-useuncs2="False"
+useuncs2="True"
 reinimode1="pred-RND"
-reinimode2="pred-DEV"
+reinimode2="pred-UNC"
 
 
 # ----------------------------------------------------------------------------
@@ -90,8 +90,8 @@ do
 	#./icann_subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$reinimode1" "$zfactors" "$d" &
 	#./icann_subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$reinimode2" "$zfactors" "$d" &
 	# SRR
-	sbatch --mem=8G --job-name="d$d-rnd" --output="slurm_d$d-rnd.%j.out" --error="slurm_d$d-rnd.%j.err" icann_subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$reinimode1" "$zfactors" "$d" &
-	sbatch --mem=8G --job-name="d$d-dev" --output="slurm_d$d-dev.%j.out" --error="slurm_d$d-dev.%j.err" icann_subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$reinimode2" "$zfactors" "$d" &
+	#sbatch --mem=8G --job-name="d$d-rnd" --output="slurm_d$d-rnd.%j.out" --error="slurm_d$d-rnd.%j.err" icann_subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$reinimode1" "$zfactors" "$d" &
+	sbatch --mem=32G --job-name="d$d-tunc" --output="slurm_d$d-tunc.%j.out" --error="slurm_d$d-tunc.%j.err" icann_subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$reinimode2" "$zfactors" "$d" &
 	# MPB?	
 	#sbatch --mem=16G --job-name="d$d-mpbkal" --output="slurm_d$d-mpbkal.%j.out" --error="slurm_d$d-mpbkal.%j.err" icann_subscript.job "$pred1" "$algnameaddition1" "$useuncs1" "$reinimode1" "$zfactors" "$d" &
 	#sbatch --mem=16G --job-name="d$d-mpbunc" --output="slurm_d$d-mpbunc.%j.out" --error="slurm_d$d-mpbunc.%j.err" icann_subscript.job "$pred2" "$algnameaddition2" "$useuncs2" "$reinimode2" "$zfactors" "$d" &
